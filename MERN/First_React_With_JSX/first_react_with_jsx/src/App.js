5de11c66f3cb11fb4d79ae4a711e4563f17d80e9
fhindo <mjhindo@gmail.com>
2022-07-06 22:17:00 -0700

    first react wuth jsx

---
+++ MERN/First_React_With_JSX/first_react_with_jsx/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // <div className="App">
+    <>
+      <h1>Hello Dojo!</h1>
+      <h1>Things I need to do:</h1>
+      <ul>
+        <li>Learn React</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </>
+    // </div>
+  );
+}
+
+export default App;
