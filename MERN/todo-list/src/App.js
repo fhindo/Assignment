c229b094e3653e02304e39a769a1ec4e59baee35
fhindo <mjhindo@gmail.com>
2022-07-11 19:37:01 -0700

    W2D1 todo-list

---
+++ MERN/todo-list/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ToDoFrom from './componenets/ToDoForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>To Do List</h1>
+      <ToDoFrom></ToDoFrom>
+    </div>
+  );
+}
+
+export default App;
---
+++ MERN/todo-list/src/componenets/ToDoForm.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { useState } from "react";
+
+
+const ToDoFrom = () => {
+    let [addAList, setAddAList] = useState("");
+    let [allLists, setAllLists] = useState([]);
+    let [checkedItem, setCheckedItem] = useState(false);
+
+    //add an item to the list
+    const addToList = (e) => {
+        e.preventDefault();
+        let newList = { addAList, checkedItem };
+        console.log(newList);
+        setAllLists([...allLists, newList]);
+        console.log(allLists);
+    }
+
+    //delete an item form the list
+    const deleteItem = (e, index) => {
+        console.log("delete an item", e, index)
+        let fiterAndRemove = allLists.filter((oneItem, idx) => {
+            return idx != index;
+        })
+        setAllLists(fiterAndRemove);
+    }
+
+    //change the style to add a line throght the p tage 
+    const checkOnOff = (e, index) => {
+        console.log("check box test", e, index);
+        let copyAllLists = [...allLists];
+        copyAllLists[index].checkedItem = e.target.checked;
+        setAllLists(copyAllLists);
+    }
+
+    return (
+        <div>
+            <form onSubmit={addToList}>
+                <div>
+                    <label>Add an item to the list:</label><br></br>
+                    <input type="text" onChange={(e) => setAddAList(e.target.value)}></input>
+                </div>
+                <div>
+                    <button type="submit">Add</button>
+                </div>
+            </form>
+
+            {/* ########## */}
+
+            <hr></hr>
+            <h2> List Items:</h2>
+            <div className="main-div">
+                {
+                    allLists.map((oneList, index) => {
+                        return (
+                            <div key={index}>
+                                {/* show item with or without line */}
+                                <p className="par" style={{ textDecorationLine: oneList.checkedItem ? 'line-through' : "" }}>
+                                    {oneList.addAList}
+                                </p>
+
+                                {/* check if the item check box is true or false and display it accordingly */}
+                                {oneList.checkedItem ?
+                                    <input type="checkbox" onChange={(e) => checkOnOff(e, index)} checked /> :
+                                    <input type="checkbox" onChange={(e) => checkOnOff(e, index)} />}
+                                
+                                {/* delete item */}
+                                <button onClick={(e) => deleteItem(e, index)}>Del</button>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+export default ToDoFrom;
