b203cecc09e15461090b1cd18978423cbfe211b0
fhindo <mjhindo@gmail.com>
2022-07-31 16:23:37 -0700

    Csharp W1D1 update

--- Csharp/Fundamentals_I/Program.cs
+++ Csharp/Fundamentals_I/Program.cs
@@ -13,21 +13,25 @@
 //     }
 // }
 
-// for (int i = 1; i <= 100; i++)
-// {
-//     if ((i * 3) % 3 == 0)
-//     {
-//         Console.WriteLine($"Fizz {i}");
-//     }
-//     if ((i * 5) % 3 == 0)
-//     {
-//         Console.WriteLine($"Buzz {i}");
-//     }
-//     if ((i * 3) % 3 == 0 && (i * 5) % 3 == 0)
-//     {
-//         Console.WriteLine($"FizzBuzz {i}");
-//     }
-// }
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine($"FizzBuzz {i}");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"Fizz {i}");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine($"Buzz {i}");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
 
 // int i = 1;
 // while (i <= 255)
