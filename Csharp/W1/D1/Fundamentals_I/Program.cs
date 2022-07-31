ac02a10c389d1fdf517dce92d0a94aeb83dcdb36
fhindo <mjhindo@gmail.com>
2022-07-31 15:55:43 -0700

    CSharp W1D1

---
+++ Csharp/W1/D1/Fundamentals_I/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// for (int i = 1; i <= 255; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+// for (int i = 1; i <= 100; i++)
+// {
+//     if (i % 3 == 0 || i % 5 == 0)
+//     {
+//         Console.WriteLine(i);
+//     }
+// }
+
+// for (int i = 1; i <= 100; i++)
+// {
+//     if ((i * 3) % 3 == 0)
+//     {
+//         Console.WriteLine($"Fizz {i}");
+//     }
+//     if ((i * 5) % 3 == 0)
+//     {
+//         Console.WriteLine($"Buzz {i}");
+//     }
+//     if ((i * 3) % 3 == 0 && (i * 5) % 3 == 0)
+//     {
+//         Console.WriteLine($"FizzBuzz {i}");
+//     }
+// }
+
+// int i = 1;
+// while (i <= 255)
+// {
+//     Console.WriteLine(i);
+//     i++;
+// }
+
+// int i = 1;
+// while (i < 100)
+// {
+//     if (i % 3 == 0 || i % 5 == 0)
+//     {
+//         Console.WriteLine(i);
+//     }
+//     i++;
+// }
