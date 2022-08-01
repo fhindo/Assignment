45a01ae7d5c7410565f2c20600f83ce6ccabe169
fhindo <mjhindo@gmail.com>
2022-08-01 13:52:02 -0700

    W1D1 Csharp boxing_unboxing

---
+++ Csharp/boxing_unboxing/Program.cs
@@ -0,0 +1,19 @@
+﻿List<object> BoxingUnBoxing = new List<object>(){
+    7, 28, -1, true, "chair"
+};
+
+int sum = 0;
+foreach (object item in BoxingUnBoxing)
+{
+    // Console.WriteLine(item);
+    if (item is int)
+    {
+        Console.WriteLine($"only the int obj: {item}");
+        // Console.WriteLine(item.GetType());
+        int num = (int)item;
+        sum = sum + num;
+        // List<int> numList = new List<int>();
+        // numList.Add(num);
+    }
+}
+Console.WriteLine($"the total of all int in the list is: {sum}");
