234dfd3316144a9944e7410a9840245feb8c9f1c
fhindo <mjhindo@gmail.com>
2022-07-31 23:41:03 -0700

    W1D1 csharp Collections practice

---
+++ Csharp/CollectionsPractice/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] numArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+string[] nameArray = { "Tim", "Martin", "Nikki", "Sara" };
+
+bool[] checkArray = new bool[10];
+checkArray = new bool[] { true, false, true, false, true, false, true, false, true, false };
+
+List<string> iceCream = new List<string>();
+iceCream.Add("Chocolate");
+iceCream.Add("Vanilla");
+iceCream.Add("Mint Chocolate Chip");
+iceCream.Add("Cookie Dough");
+iceCream.Add("Buttered Pecan");
+
+Console.WriteLine(iceCream.Count);
+Console.WriteLine(iceCream[2]);
+iceCream.RemoveAt(2);
+Console.WriteLine(iceCream.Count);
+
+Dictionary<string, string> favIceCream = new Dictionary<string, string>();
+
+Random rand = new Random();
+favIceCream.Add(nameArray[0], iceCream[rand.Next(iceCream.Count)]);
+favIceCream.Add(nameArray[1], iceCream[rand.Next(iceCream.Count)]);
+favIceCream.Add(nameArray[2], iceCream[rand.Next(iceCream.Count)]);
+favIceCream.Add(nameArray[3], iceCream[rand.Next(iceCream.Count)]);
+
+
+foreach (var item in favIceCream)
+{
+    Console.WriteLine($"{item.Key} - {item.Value}");
+}
